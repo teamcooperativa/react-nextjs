aa6ce5e1afb3b3beba3deef0326dab26e414f818
Ivan <vancho54@gmail.com>
2020-05-04 12:43:57 -0300

    fix: correct display of date use ::before instead of border in the table

--- pages/Component/Table/Rows/Rows.js
+++ pages/Component/Table/Rows/Rows.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+const mon = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September','October','November','December']
 const Row = ({ data }) => {
   return (
     data.map(el => 
       <tr className='row' key={el.id}>
         <td key={el.title}>{el.title}</td>
-        <td key={el.date}>{new Date(el.date).toDateString()}</td>
+        <td key={el.date}>{mon[new Date(el.date).getMonth()]} {new Date(el.date).getDate()}, {new Date(el.date).getFullYear()}</td>
         <td key={el.status}>{el.status || 'Draft'}</td>
         <td key={el.status}>Actions</td>
       </tr>
