468853d81081b64b6c963ff4f0d6fca6eba8b66d
mdurand <mdurand@amr.org.ar>
2020-05-08 13:14:15 -0300

    add more styles for responsiveness

--- pages/components/table.js
+++ pages/components/table.js
@@ -19,17 +19,17 @@ var lessons = [
   }
 ]
 
-const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August','September','October','November','December']
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
 class Lessons extends React.Component {
   render() {
     return lessons.map((lesson) =>
       <tbody>
         <tr key="{lesson.id}">
-          <td>{lesson.title}</td>
-          <td>{months[new Date(lesson.date).getMonth()]} {new Date(lesson.date).getDate()}, {new Date(lesson.date).getFullYear()}</td>
-          <td>{lesson.status || 'Draft'}</td>
-          <td></td>
+          <td key={lesson.title}>{lesson.title}</td>
+          <td key={lesson.date}>{months[new Date(lesson.date).getMonth()]} {new Date(lesson.date).getDate()}, {new Date(lesson.date).getFullYear()}</td>
+          <td key={lesson.status}>{lesson.status || 'Draft'}</td>
+          <td key={lesson.status}></td>
         </tr>
       </tbody>
     );
@@ -47,7 +47,9 @@ class Table extends React.Component {
       <div className="container">
         <table>
           <thead>
-            <h3>All</h3>
+          <tr>
+            <th id="all">All</th>
+          </tr>
             <tr>
               <th>Lesson Title</th>
               <th>Date</th>
