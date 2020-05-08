fa62611d72bf4cc19e1474ab4591a8aeef638054
mdurand <mdurand@amr.org.ar>
2020-05-08 16:16:27 -0300

    fix varios

--- pages/components/table.js
+++ pages/components/table.js
@@ -3,19 +3,22 @@ var lessons = [
     id: 1,
     title: "lesson 1",
     date: "2020-04-21T15:52:14.565Z",
-    status: ""
+    status: "",
+    actions: ""
   },
   {
     id: 2,
     title: "lesson 2",
     date: "2020-04-22T15:52:14.565Z",
-    status: ""
+    status: "",
+    actions: ""
   },
   {
     id: 3,
     title: "lesson 3",
     date: "2020-04-23T15:52:14.565Z",
-    status: ""
+    status: "",
+    actions: ""
   }
 ]
 
@@ -25,7 +28,7 @@ class Lessons extends React.Component {
   render() {
     return lessons.map((lesson) =>
       <tbody>
-        <tr key="{lesson.id}">
+        <tr key={lesson.id}>
           <td key={lesson.title}>{lesson.title}</td>
           <td key={lesson.date}>{months[new Date(lesson.date).getMonth()]} {new Date(lesson.date).getDate()}, {new Date(lesson.date).getFullYear()}</td>
           <td key={lesson.status}>{lesson.status || 'Draft'}</td>
@@ -38,18 +41,12 @@ class Lessons extends React.Component {
 
 class Table extends React.Component {
 
-  renderLessons() {
-    return <Lessons />;
-  }
-
   render() {
     return (
       <div className="container">
         <table>
           <thead>
-          <tr>
-            <th id="all">All</th>
-          </tr>
+          <tr><th id="all">All</th></tr>
             <tr>
               <th>Lesson Title</th>
               <th>Date</th>
@@ -57,7 +54,7 @@ class Table extends React.Component {
               <th>Actions</th>
             </tr>
           </thead>
-          {this.renderLessons()}
+          <Lessons />
         </table>
       </div>
     );
