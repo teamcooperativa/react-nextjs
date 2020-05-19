cf0f56a5ade71f8bb40848f7de05844b3423eda4
nanodm <nanodurandm@gmail.com>
2020-05-19 20:25:47 -0300

    refactor class components into functional components

--- pages/components/table.js
+++ pages/components/table.js
@@ -21,41 +21,36 @@ var lessons = [
 
 const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 
-class Lessons extends React.Component {
-  render() {
-    return lessons.map((lesson) =>
-      <tbody>
-        <tr key={lesson.id}>
-          <td key={lesson.title}>{lesson.title}</td>
-          <td key={lesson.date}>{months[new Date(lesson.date).getMonth()]} {new Date(lesson.date).getDate()}, {new Date(lesson.date).getFullYear()}</td>
-          <td key={lesson.status}>{lesson.status || 'Draft'}</td>
-          <td key={lesson.status}></td>
-        </tr>
-      </tbody>
-    );
-  }
+function Lessons() {
+  return lessons.map((lesson) =>
+    <tbody>
+      <tr key={lesson.id}>
+        <td key={lesson.title}>{lesson.title}</td>
+        <td key={lesson.date}>{months[new Date(lesson.date).getMonth()]} {new Date(lesson.date).getDate()}, {new Date(lesson.date).getFullYear()}</td>
+        <td key={lesson.status}>{lesson.status || 'Draft'}</td>
+        <td key={lesson.status}></td>
+      </tr>
+    </tbody>
+  );
 }
 
-class Table extends React.Component {
-
-  render() {
-    return (
-      <div className="container">
-        <table>
-          <thead>
+function Table() {
+  return (
+    <div className="container">
+      <table>
+        <thead>
           <tr><th id="all">All</th></tr>
-            <tr>
-              <th>Lesson Title</th>
-              <th>Date</th>
-              <th>Status</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <Lessons />
-        </table>
-      </div>
-    );
-  }
+          <tr>
+            <th>Lesson Title</th>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <Lessons />
+      </table>
+    </div>
+  );
 }
 
 export default Table;
